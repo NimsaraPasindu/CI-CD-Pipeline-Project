a51e5f5b5243c8130c8be83da0cafb99f421618d
NimsaraPasindu <nimsarapasindu63@gmail.com>
2025-05-22 02:27:18 +0530

    cypress delete

--- cypress/integration/home.spec.js
+++
@@ -1,7 +0,0 @@
-//// cypress/integration/home.spec.js
-describe('Home Page', () => {
-  it('loads successfully and shows title', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('Welcome to MyApp')
-  });
-});
